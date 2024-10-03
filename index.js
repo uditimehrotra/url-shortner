b01e4b11b22396383defa8f2da099d91b23df70e
uditi <uditimehrotra06@gmail.com>
2024-10-03 12:30:00 +0530

    upload file multer

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const multer  = require('multer');
+
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const staticRoute = require('./routes/staticRouter');
@@ -7,6 +9,17 @@ const userRoute = require("./routes/user");
 const app = express();
 const {connectToMongoDB} = require('./connect');
 const PORT =8004;
+// const upload = multer({ dest: 'uploads/' })
+const storage= multer.diskStorage({
+    destination: function(req,file,cb){
+        return cb(null,"./uploads");
+    },
+    filename: function(req,file,cb){
+        return cb(null,`${Date.now()}-${file.originalname}`);
+    },
+});
+
+const upload = multer({storage});
 const {checkForAuthentication,restrictTo} = require("./middlewares/auth");
 connectToMongoDB('mongodb://localhost:27017/short-url').then(()=>console.log('connected db'));
 
@@ -27,6 +40,11 @@ app.use("/user",userRoute);
 //     });
 // });
 
+app.post('/upload',upload.single('profileImage'),(req,res)=>{
+   // console.log(req.body);
+  //console.log(req.file);
+    return res.render("home");
+})
 app.use("/url",restrictTo(["NORMAL","ADMIN"]),urlRoute);
 app.use("/discord",urlRoute);
 app.listen(PORT,()=> console.log(`server started`));
